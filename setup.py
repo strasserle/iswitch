18c18a4a93c55907b1af5dcb7de43b49ac19ba52
Lena <strasserl@cip.ifi.lmu.de>
2021-05-29 19:18:36 +0200

    spada

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='iswitch',
+setup(name='iswitchpy',
       version='0.0',
       description='Isoform switch detection',
       url='http://github.com/strasserle/iswitch',
       author='Lena Strasser',
-      packages=['iswitch'],
+      packages=['iswitchpy'],
       zip_safe=False)
